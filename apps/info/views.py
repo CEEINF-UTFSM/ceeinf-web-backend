36c33563fab141ce1b84a49f1b74e94cb1400d4e
LaTortugaR <vicente.figueroa@sansano.usm.cl>
2020-11-05 22:09:15 -0300

    Añadido ramos y ayudantias

--- apps/info/views.py
+++ apps/info/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 class ConvalidationsView(TemplateView):
 
     template_name = "convalidations.html"
+
+class SubjectsView(TemplateView):
+
+    template_name = "subjects_and_assistantships.html"
