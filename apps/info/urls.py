f114f3b8c01e204f4155db04f3d15f5431b46b03
LaTortugaR <vicente.figueroa@sansano.usm.cl>
2020-11-12 15:50:21 -0300

    Noticias
    -added news.html
    -added news_view (scraping)
    -changed requirements (+bs4)

--- apps/info/urls.py
+++ apps/info/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ConvalidationsView, SubjectsView
+from .views import ConvalidationsView, SubjectsView, news_view
 
 
 urlpatterns = [
-    path("convalidaciones",  ConvalidationsView.as_view())
-    path("ramos-y-ayudantias", SubjectsView.as_view())
+    path("convalidaciones",  ConvalidationsView.as_view()),
+    path("ramos-y-ayudantias", SubjectsView.as_view()),
+    path("noticias", news_view),
 ]
